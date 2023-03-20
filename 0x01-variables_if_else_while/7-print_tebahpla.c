cd771985a875d2ad240a1d44e90fdc6dea9e01aa
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-03-20 08:27:58 -0700

    c coding is enjoyable

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char ch;
 
+	for (ch = 'z'; ch >= 'a'; ch--)
+
+		putchar(ch);
+
+	putchar('\n');
+	return (0);
+}
