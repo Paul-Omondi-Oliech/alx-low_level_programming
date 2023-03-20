324082602bca093dd91a672343575c0244493c0f
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-03-20 08:36:09 -0700

    c coding is enjoyable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 /**
  * main - Prints all possible combinations of two two-digit numbers.
  *
- * Return:
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -14,10 +14,10 @@ int main(void)
 
 			if (i < j)
 				putchar(i / 10 + 48);
-	putchar(i % 10 + 48);
+	putchar((i % 10) + 48);
 	putchar(' ');
-	putchar(j / 10 + 48);
-	putchar(j % 10 + 48);
+	putchar((j / 10) + 48);
+	putchar((j % 10) + 48);
 	if (i != 98 || j != 99)
 
 		putchar(',');
