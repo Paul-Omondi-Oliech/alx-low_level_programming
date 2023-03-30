b650556d588afb1342c0879401115d867ae04611
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-03-30 04:27:30 -0700

    update 2-alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * print_alphabet_x10 - prints 10 times the alphabet, in lower case.
  * followed by new line.
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	char ch;
 	int i;
@@ -12,13 +12,13 @@ void print_alphabet_x10(void);
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('\n');
-		i++
+	ch = 'a';
+	while (ch <= 'z')
+	{
+	_putchar(ch);
+	ch++;
+	}
+	_putchar('\n');
+	i++;
 	}
 }
