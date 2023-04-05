e23bb657dae12c85e75c4518d334c92b0ba28aba
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-04-05 07:10:29 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	int longint = 0
+	int longit = 0;
 
 	if (*s)
 	{
-		longint++;
-		longint += _strlen_recursion(s + 1);
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	return (longint);
+
+	return (longit);
 }
