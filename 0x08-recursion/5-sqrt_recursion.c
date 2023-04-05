373414996b98a0caaf689bd82ec9db40bbe9800d
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-04-05 09:08:55 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,5 +11,19 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == n * n)
 		return (1);
-	return (_sqrt_recursion(n * n));
+	return (_sqrt_recursion(n));
 			}
+/**
+ * actual_sqrt_recursion - finds the natural square root of a number
+ * @n: number whose square root is to be found
+ * @i: the iterator
+ * Return: the square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
