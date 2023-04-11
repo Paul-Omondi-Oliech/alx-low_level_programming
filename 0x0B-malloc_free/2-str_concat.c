795613533a0dc4df9ebe7f304775cbd76e771f46
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-04-11 14:16:46 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 
-		s1 = "";
+	s1 = "";
 
 	if (s2 == NULL)
 
-		s2 = "";
-		i = ci = 0;
+	s2 = "";
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 		i++;
@@ -30,8 +30,8 @@ char *str_concat(char *s1, char *s2)
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
-		return (NULL);
-		i = ci = 0;
+	return (NULL);
+	i = ci = 0;
 
 	while (s1[i] != '\0')
 
