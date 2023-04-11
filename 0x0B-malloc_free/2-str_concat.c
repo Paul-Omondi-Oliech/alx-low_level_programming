bd367d03ca332a321f0e991755b58fde829eb4c0
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-04-11 14:10:10 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,6 +7,7 @@
  * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
+
 {
 	char *conct;
 	int i, ci;
@@ -29,22 +30,22 @@ char *str_concat(char *s1, char *s2)
 	conct = malloc(sizeof(char) * (i + ci + 1));
 
 	if (conct == NULL)
-
 		return (NULL);
-
 		i = ci = 0;
 
 	while (s1[i] != '\0')
+
 	{
-		conct[i] = s1[i];
-		i++;
+	conct[i] = s1[i];
+	i++;
 	}
+
 	while (s2[ci] != '\0')
 	{
-		conct[i] = s2[ci];
-		i++, ci++;
+	conct[i] = s2[ci];
+	i++, ci++;
 	}
-	conct[i] = '\0';
 
+	conct[i] = '\0';
 	return (conct);
 }
