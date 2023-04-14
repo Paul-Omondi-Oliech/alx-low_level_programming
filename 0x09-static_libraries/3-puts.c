33fa98fc115dd579ac5c85edcba46492114ad1f0
Paul-Omondi-Oliech <paulomondioyoo@gmail.com>
2023-04-14 00:58:29 -0700

    create_static_lib.sh update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,11 +1,13 @@
 #include "main.h"
-#include<stdio.h>
 /**
- * _puts - Displays the string
- *@s: input
- * Return: Always 0 (Success)
+ * _puts - prints a string, followed by a new line, to stdout
+ * @s: string to print
  */
 void _puts(char *s)
 {
-
+	while (*s != '\0')
+	{
+		_putchar(*s++);
+	}
+		_putchar('\n');
 }
--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -8,5 +8,10 @@
  */
 int _abs(int n)
 {
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 
 }
